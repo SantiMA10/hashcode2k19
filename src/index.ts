7c06cbdae3a305d6b802aaebce601008028a6a8b
Santi <santiagomartinagra@gmail.com>
2019-01-21 20:25:57 +0100

    feat: solves c_medium

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 
 const debug = true;
-const data = fs.readFileSync(`${__dirname}/../inputs/b_small.in`, 'utf8').split('\n');
+const data = fs.readFileSync(`${__dirname}/../inputs/c_medium.in`, 'utf8').split('\n');
 
 // input constants
 const [ R, C, L, H ] = data[0].split(' ').map(n => parseInt(n));
@@ -9,16 +9,15 @@ if (debug) console.log({R, C, L, H})
 
 // pizza
 const pizza = data.slice(1, R + 1)
-if (debug) console.log(pizza)
+// if (debug) console.log(pizza)
 
 // algoritm
 const solutions = []
 for (let i = 0; i < R; i++) {
     const row = pizza[i].split('');
-    if (debug) console.log({row})
+    // if (debug) console.log({row})
 
     let beg = 0;
-    let end = 0;
     let mushrooms = 0;
     let tomatoes = 0;
 
@@ -31,15 +30,15 @@ for (let i = 0; i < R; i++) {
             mushrooms++;
         }
 
-        if (tomatoes >= L && mushrooms >= L) {
+        if (tomatoes >= L && mushrooms >= L && tomatoes + mushrooms < H) {
+            solutions.push(`${i} ${beg} ${i} ${j}`)
+            if (debug && solutions.length === 257) console.log('slice! 🍕 ', `${i} ${beg} ${i} ${j}`, {mushrooms, tomatoes})
             mushrooms = 0;
             tomatoes = 0;
-            solutions.push(`${i} ${beg} ${i} ${j}`)
-            if (debug) console.log('slice! 🍕 ', `${i} ${beg} ${i} ${j}`)
             beg = j + 1;
         }
 
-        if (tomatoes + mushrooms === H) {
+        if (tomatoes + mushrooms === H - 1) {
             mushrooms = 0;
             tomatoes = 0;
             beg = j;
@@ -52,4 +51,4 @@ const solution = solutions.reduce((text, solution) => {
 }, `${solutions.length}\n`)
 
 // generate output
-fs.writeFileSync(`${__dirname}/../outputs/b_small.out`, solution, 'utf8')
+fs.writeFileSync(`${__dirname}/../outputs/c_medium.out`, solution, 'utf8')
